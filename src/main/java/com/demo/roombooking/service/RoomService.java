a7e75bfae1bdeb4767d8e8270b10c78844fff633
LicheeGod <13433624639@163.com>
2019-07-07 16:13:53 +0800

    first commit

---
+++ src/main/java/com/demo/roombooking/service/RoomService.java
@@ -0,0 +1,12 @@
+package com.demo.roombooking.service;
+
+
+import com.demo.roombooking.common.resp.JsonResponse;
+import com.demo.roombooking.entity.Room;
+
+public interface RoomService{
+    JsonResponse AddRoom(Room room);
+
+
+
+}
